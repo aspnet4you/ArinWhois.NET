32345d89b9066aebacf8b0717beeb3297e2d2885
Prodip K Saha <prodip.kumar@outlook.com>
2020-08-01 18:45:24 -0500

    ArinWhois project target framework upgraded to netcoreapp3.1; project output to library; newtonsoft upgraded to 12.0.3.
    Unit Test project upgraded to netcoreapp3.1
    
    Added a new project asp.net core api. This is the default project now. With this change you can deploy the application on multiple OS/Platform including Docker Container.

---
+++ src/ArinWhoisAspnetCore/Controllers/WhoIsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using ArinWhois.Client;
+using ArinWhois.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ArinWhoisAspnetCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WhoIsController : ControllerBase
+    {
+
+        private readonly ILogger<WhoIsController> _logger;
+
+        public WhoIsController(ILogger<WhoIsController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Find WhoIs for the IP Address
+        /// </summary>
+        /// <param name="partitionKey"></param>
+        /// <returns>List of AzCountryProvince</returns>
+        [HttpGet("WhoIsIP")]
+        public Response WhoIsIP(string ipAddress)
+        {
+            var arinClient = new ArinClient();
+            var ipResponse = arinClient.QueryIpAsync(IPAddress.Parse(ipAddress)).Result;
+            return ipResponse;
+        }
+    }
+}
---
+++ src/ArinWhoisAspnetCore/Startup.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace ArinWhoisAspnetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ARIN WhoIs API at ASPNET4YOU",
+                    Description = "ARIN WhoIs API at ASPNET4YOU",
+                    TermsOfService = new Uri("https://swagger.io/license/"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Prodip K Saha",
+                        Email = "",
+                        Url = new Uri("https://blogs.aspnet4you.com/about/")
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ARIN WhoIs API at ASPNET4YOU v1");
+            });
+        }
+    }
+}
